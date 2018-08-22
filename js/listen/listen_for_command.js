8b968cf1ee8a647e84d4baa14be1fb39006ab1a4
Tyler Carpenter <tjcarpenter15@my.trine.edu>
2018-08-21 20:14:49 -0400

    added functionality to listen for command

--- js/listen/listen_for_command.js
+++ js/listen/listen_for_command.js
@@ -1,4 +1,83 @@
-function listen_for_command() {
+// listen_for_command.js
 
-    
+// Function:
+/*
+    Await user speech. Determine the command that was spoken and execute the specific command. If the user does not
+    state a command after 5 seconds, abort and go back to listening for the trigger.
+*/
+
+function listen_for_command() {
+    // Check if browser supports SpeechRecognition and SpeechSynthesis
+    if (!(window.webkitSpeechRecognition) && !(window.speechRecognition) && !(window.speechSynthesis)) {
+        upgrade();
+    } else {
+
+        // Initialize SpeechRecognition and SpeechSynthesis
+        var speech = new webkitSpeechRecognition() || speechRecognition();
+        var synth = window.speechSynthesis;
+
+        // Initialize recognizing state
+        var recognizing;
+
+        // Reset Function
+        function reset() {
+            recognizing = false;
+            speech.start()
+        }
+
+        speech.continuous = true;
+        speech.interimResults = true;
+        speech.lang = 'en-US'
+        speech.start();
+
+        speech.onstart = function () {
+            // When recognition begins
+            recognizing = true;
+        };
+
+        speech.onresult = function (event) {
+            // When recognition produces a result
+            var interim_transcript = '';
+            var final_transcript = '';
+
+            // main for loop for the final and interem results
+            for (var i = event.resultIndex; i < event.results.length; ++i) {
+                if (event.results[i].isFinal) {
+                    final_transcript += event.results[i][0].transcript;
+                } else {
+                    interim_transcript += event.results[i][0].transcript;
+                }
+            }
+
+            // Determine if the command is a valid command
+            if (validCommand()) {
+
+
+            }
+
+                // speech.abort();
+                // recognizing = false;
+                // trigger_heard = true;
+
+                // speak(respond_to_greeting);
+
+        };
+
+        const speak = (action) => {
+            utterThis = new SpeechSynthesisUtterance(action());
+            synth.speak(utterThis);
+        };
+
+        speech.onerror = function (event) {
+            // Either 'No-speech' or 'Network connection error'
+            console.error(event.error);
+        };
+
+        speech.onend = function () {
+            // When recognition end
+            closing_response();
+            listen_for_trigger();
+        };
+    }
+
 }
---
+++ js/response/closing_response.js
@@ -0,0 +1,11 @@
+// closing_response.js
+
+// Function:
+/*
+    Create a response and then send back a response string
+*/
+
+function closing_response() {
+    const response = 'brr';
+    return response;
+}
